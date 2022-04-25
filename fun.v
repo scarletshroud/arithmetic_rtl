6b3506ddbbedcd761d398285e3b3d444fc2182db
slayer404 <doomer888@protonmail.com>
2022-04-25 13:31:58 +0300

    general function

---
+++ fun.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.04.2022 13:26:53
+// Design Name: 
+// Module Name: fun
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fun(
+
+    );
+endmodule
